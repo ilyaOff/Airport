7986628959bfe322e1d808ef9fbae7e0c044ecba
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2021-04-01 12:49:30 +0300

    Старт

---
+++ Airoport/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Airoport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbShedule = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbShedule
+            // 
+            this.tbShedule.Location = new System.Drawing.Point(56, 82);
+            this.tbShedule.Name = "tbShedule";
+            this.tbShedule.Size = new System.Drawing.Size(100, 22);
+            this.tbShedule.TabIndex = 0;
+            this.tbShedule.Text = "Путь до файла";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Расписание:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbShedule);
+            this.Name = "Form1";
+            this.Text = "Стартовое окно";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbShedule;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
